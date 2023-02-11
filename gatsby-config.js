7574eb756e967073be317678223133a8fa1f03cc
Aharon <grigoryanaharon434@gmail.com>
2023-02-11 19:58:16 +0400

    created home page he design

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 /**
  * Configure your Gatsby site with this file.
  *
@@ -7,6 +10,50 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+// const queries = require("./src/constants/algolia")
 module.exports = {
-  plugins: [],
-}
+  siteMetadata: {
+    title: `Design Shop`,
+    description: `Gatsby Airtable Example. Built using Airtable, Algolia Search, Gatsby Background Image plugin and  React Context API. Containts two sliders, real-time Airtable updates and submenus. Styled using Styled-Components. `,
+    author: `@johnsmilga`,
+    titleTemplate: `%s | Gatsby - Airtable`,
+    url: `https://gatsby-airtable-design-project.netlify.app/`,
+    image: `mainBcg.png`,
+    twitterUsername: `@john_smilga`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API, // may instead specify via env, see below
+        concurrency: 5, // default, see using markdown and attachments for more information
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Projects`,
+            mapping: { image: `fileNode` },
+          },
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Customers`,
+            mapping: { image: `fileNode` },
+          },
+        ],
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,79 @@
-import * as React from "react"
+import React from "react";
+import { graphql } from "gatsby";
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from "../components";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const HomePage = ({ data }) => {
+  const {
+    allAirtable: { nodes: projects },
+    customers: { nodes },
+  } = data;
+  return (
+    <Layout>
+      <Hero />
+      <About />
+      <Projects projects={projects} title="Latest projects" />
+      <Slider customers={nodes} />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 5
+      sort: { data: { Name: DESC } }
+    ) {
+      nodes {
+        id
+        data {
+          date
+          Name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(
+                  layout: CONSTRAINED
+                  placeholder: DOMINANT_COLOR
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+    customers: allAirtable(filter: { table: { eq: "Customers" } }) {
+      nodes {
+        data {
+          Name
+          quote
+          title
+          image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(
+                  layout: FIXED
+                  placeholder: DOMINANT_COLOR
+                  width: 150
+                  height: 150
+                )
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`;
+
+export default HomePage;
